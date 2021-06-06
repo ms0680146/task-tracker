bb7a9597b58cc4ca55f1a3baa21e6dc93cf7ff45
Andy <ms0680146@gmail.com>
2021-06-06 19:28:37 +0800

    feat: submit form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -34,9 +35,16 @@ function App() {
     ))
   }
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className='container'>
       <Header title={'Task Tracker'} />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
       ) : (
